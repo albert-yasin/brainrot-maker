fce49ca09e1b4f82b19d5aa1b212a5e4cc93c4cd
Albert <albert.yasin@japfa.com>
2025-02-11 16:52:19 +0700

    dynamic prompt and adding fetch animation for title, caption and script

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -51,13 +51,59 @@
     >
       <v-card class="generated-form mt-5 pa-5" v-if="showGeneratedContent">
         <v-card-content>
-          <v-textarea
-            v-model="generatedData"
-            variant="outlined"
-            :loading="generatedData == null"
-            :disabled="generatedData == null"
-          >
-          </v-textarea>
+          <v-row>
+            <v-col cols="12">
+              <v-textarea
+                v-model="generatedData.title.data"
+                variant="outlined"
+                :label="
+                  generatedData.title.data == null &&
+                  generatedData.title.fetching == true
+                    ? 'Generating ' + generatedData.title.label + '...'
+                    : generatedData.title.label
+                "
+                :loading="generatedData.title.fetching == true"
+                :disabled="generatedData.title.data == null"
+              ></v-textarea>
+            </v-col>
+            <v-col cols="12">
+              <v-textarea
+                v-model="generatedData.caption.data"
+                variant="outlined"
+                :label="
+                  generatedData.title.data == null &&
+                  generatedData.caption.fetching == true
+                    ? 'Generating ' + generatedData.caption.label + '...'
+                    : generatedData.caption.label
+                "
+                :loading="generatedData.caption.fetching == true"
+                :disabled="generatedData.caption.data == null"
+              >
+              </v-textarea>
+            </v-col>
+            <v-col cols="12">
+              <v-textarea
+                v-model="generatedData.script.data"
+                variant="outlined"
+                :label="
+                  generatedData.title.data == null &&
+                  generatedData.title.fetching == true
+                    ? 'Generating ' + generatedData.script.label + '...'
+                    : generatedData.script.label
+                "
+                :loading="generatedData.script.fetching == true"
+                :disabled="generatedData.script.data == null"
+              >
+              </v-textarea>
+              <v-list
+                class="align-left d-flex"
+                v-for="script in JSON.parse(generatedData.script.data)"
+                :key="script.timestamp"
+              >
+                <span>{{ script.timestamp }} - {{ script.narator }} </span>
+              </v-list>
+            </v-col>
+          </v-row>
         </v-card-content>
         <v-btn color="primary" variant="outlined" @click="resetForm"
           >Generate another one</v-btn
@@ -86,7 +132,24 @@
   const generateContentForm = ref(null)
 
   //api
-  const generatedData = ref(null)
+  const generatedData = ref({
+    title: {
+      label: 'Title',
+      data: null,
+      fetching: false,
+    },
+    script: {
+      label: 'Script',
+      data: null,
+      fetching: false,
+    },
+    caption: {
+      label: 'Caption',
+      data: null,
+      fetching: false,
+    },
+  })
+
   //states
   const showGenerateForm = ref(true)
   const showGeneratedContent = ref(false)
@@ -95,10 +158,35 @@
     const valid = await generateContentForm.value.validate()
 
     if (valid.valid == true) {
+      generatedData.value.title.fetching = true
       showGenerateForm.value = false
-      const prompt = `Generate me a ${duration.value} seconds story about ${wdyw.value}`
+      const prompt = `In JSON data not wrapped with anything output generate me a content about  ${wdyw.value}, and fill these parameters title, script with only timestamp (minutes:seconds) and narator for ${duration.value} seconds, and caption`
+
       const result = await model.generateContent(prompt)
-      generatedData.value = result.response.text()
+      //remove markdown code block from gemini response
+      const formattedResponse = result.response
+        .text()
+        .replace('```json', '')
+        .replace('```', '')
+      //convert to JSON
+      const generated = JSON.parse(formattedResponse)
+
+      setTimeout(() => {
+        generatedData.value.title.data = generated.title
+        generatedData.value.title.fetching = false
+        generatedData.value.caption.fetching = true
+      }, 1000)
+
+      setTimeout(() => {
+        generatedData.value.caption.data = generated.caption
+        generatedData.value.caption.fetching = false
+        generatedData.value.script.fetching = true
+      }, 2000)
+
+      setTimeout(() => {
+        generatedData.value.script.data = JSON.stringify(generated.script)
+        generatedData.value.script.fetching = false
+      }, 3000)
     }
   }
 
@@ -106,7 +194,9 @@
     wdyw.value = ''
     bgm.value = ''
     duration.value = null
-    generatedData.value = ''
+    generatedData.value.title.data = null
+    generatedData.value.script.data = null
+    generatedData.value.caption.data = null
     showGeneratedContent.value = false
   }
 
